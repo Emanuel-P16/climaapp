ed1ac791dd438d05ede8d2455146d32896efc7e0
Emanuel-P16 <77447383+Emanuel-P16@users.noreply.github.com>
2022-01-25 06:43:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const { Busquedas } = require("./models/busquedas");
+
+
+const main = async () => {
+    
+    const busquedas = new Busquedas()
+
+   
+    do {
+        opt = await inquirerMenu()
+       
+
+        switch (opt){
+            case 1:
+                // mostrar mensaje
+                const lugar = await leerInput('Ciudad :')
+                busquedas.ciudad(lugar)
+
+                // buscar los lugares
+
+                // seleccionar el lugar
+
+                // Clima
+
+                // Mostrar resultados
+                console.log('\nInformación de la ciudad \n'.green)
+                console.log('Ciudad:',)
+                console.log('Lat:',)
+                console.log('Lng:',)
+                console.log('Temperatura:',)
+                console.log('Mínima:',)
+                console.log('Maxima:',)
+
+            break;
+            await pausa()
+        }
+        if (opt !== 0) await pausa()
+    } while (opt !== 0);
+}
+
+main()
