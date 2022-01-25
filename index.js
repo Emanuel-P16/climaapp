3a095aa4a2c0d5ce16075d2a4f67f7642ff6bd2c
Emanuel-P16 <palazon.emanuel@gmail.com>
2022-01-25 16:36:52 -0300

    proyecto terminado

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+require('dotenv').config()
+const { leerInput, inquirerMenu, pausa,listarLugares } = require("./helpers/inquirer");
 const { Busquedas } = require("./models/busquedas");
 
 
 const main = async () => {
-    asdasdasd
+    
     const busquedas = new Busquedas()
 
    
@@ -14,26 +15,47 @@ const main = async () => {
         switch (opt){
             case 1:
                 // mostrar mensaje
-                const lugar = await leerInput('Ciudad :')
-                busquedas.ciudad(lugar)
+                const termino = await leerInput('Ciudad :')
+               
+               
+
 
                 // buscar los lugares
-
+                const lugares = await busquedas.ciudad(termino)
+               
                 // seleccionar el lugar
+                const id = await listarLugares(lugares)
 
+                if (id === '0') continue
+
+                // guarar en db
+                
+                const lugarSeleccionado = lugares.find( l => l.id === id)
+                busquedas.agregarHistorial(lugarSeleccionado.nombre)
                 // Clima
-
+                const clima = await busquedas.clima(lugarSeleccionado.lng,lugarSeleccionado.lat)
+                // console.log(clima)
                 // Mostrar resultados
-                // console.log('\nInformación de la ciudad \n'.green)
-                // console.log('Ciudad:',)
-                // console.log('Lat:',)
-                // console.log('Lng:',)
-                // console.log('Temperatura:',)
-                // console.log('Mínima:',)
-                // console.log('Maxima:',)
-
+                console.log('\nInformación de la ciudad \n'.green)
+                console.log('Ciudad:',lugarSeleccionado.nombre)
+                console.log('Lat:',lugarSeleccionado.lat)
+                console.log('Lng:',lugarSeleccionado.lng,)
+                console.log('Temperatura:',clima.temp,)
+                console.log('Mínima:',clima.temp_min)
+                console.log('Maxima:',clima.temp_max)
+                console.log('Estado:',clima.desc)
             break;
-            await pausa()
+            case 2:
+                busquedas.historialCapitalizado.forEach((lugar,i) =>{
+                    const idx = `${i+1}.`.green;
+                    console.log(idx,lugar)
+                })
+                // busquedas.historial.forEach( (lugar,i) => {
+                //     const idx = `${i + 1}.`.green
+                //     console.log(idx,lugar)
+                // })
+            break;
+            
         }
         if (opt !== 0) await pausa()
     } while (opt !== 0);
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,35 +1,119 @@
-const axios = require('axios')
+
+const fs = require('fs')
+const axios = require('axios');
+
 
 class Busquedas {
     historial = [];
+    dbPath = './db/database.json'
     constructor() {
         // TODO: leer DB si existe
+        this.leerDB()
     }
 
+    get historialCapitalizado  () {
+        return this.historial.map( lugar => {
+            let palabras = lugar.split(' ');
+            palabras = palabras.map( p => p[0].toUpperCase() + p.substring(1))
+                return palabras.join(' ')
+            
+        })
+    }
+
+    get paramsMapbox () {
+        return {
+            'access_token':process.env.MAPBOX_KEY,
+            'limit':5,
+            'language':'es'
+
+        }
+    }
+
+    get paramsOpenWeather(){
+        return {
+            'units':'metric',
+            'lang':'es'
+        }
+    }
     async ciudad ( lugar = ''){
         /// peticion http 
         // console.log('Ciudad',lugar)
         try{
-            const instance = axios.create({
-                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
-                params: {
-                   'access_token': 'pk.eyJ1IjoiZWRnZXAxNiIsImEiOiJja3lzenFpdGsxOTB3MzF0ODhwanZmNDIyIn0.hplq1ySSH7DRYoSZG532eg',
-                    'limit':5,
-                    'lenguage':'es'
-                }
+            
+            const intance = axios.create({
+                baseURL: `https://api.mapbox.com/`,
+                params:this.paramsMapbox
+                
             })
-            const resp = await instance.get();
+            const resp = await intance.get(`/geocoding/v5/mapbox.places/${ lugar }.json`);
             // const respuesta = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/BUENOS%20AIRES.json?types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1IjoiZWRnZXAxNiIsImEiOiJja3lzenFpdGsxOTB3MzF0ODhwanZmNDIyIn0.hplq1ySSH7DRYoSZG532eg')
-            console.log('hola')
-            console.log(resp.data)
-            return []
+            return resp.data.features.map( lugar => ({
+                id: lugar.id,
+                nombre:lugar.place_name,
+                lng:lugar.center[0],
+                lat:lugar.center[1]
+            }))
+            
         }catch (error){
+            console.log(error)
             return []
         }
        
         // retornar los lugares que coincidan con la variable lugar
     } 
 
+    async clima (lng = '',lat = '') {
+        try{
+            const instance = axios.create({
+                baseURL:'http://api.openweathermap.org/',
+                params:this.paramsOpenWeather
+            })
+            const resp = await instance.get(`/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${process.env.OPENWEATHER_KEY}`)
+            const { weather,main} = resp.data
+
+             return {
+                 desc: weather[0].description,
+                 temp: main.temp,
+                 temp_min: main.temp_min,
+                 temp_max: main.temp_max
+
+             }
+            // return resp.data.main,resp.data.weather[0].description
+           
+        }catch (error){
+            console.log(error)
+            return []
+        }
+    }
+    agregarHistorial( lugar = ''){
+
+        /// TODO prevenir duplicados
+        /// 
+        if (this.historial.includes( lugar .toLowerCase())){
+            return;
+        } 
+
+        this.historial = this.historial.splice(0,5)
+        this.historial.unshift(lugar.toLowerCase());
+        /// grabar en db/txt
+        this.guardarDB()
+    }
+
+    guardarDB(){
+        const payload = {
+            historial:this.historial
+        }
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload))
+    }
+
+    leerDB(){
+        if (!fs.existsSync(this.dbPath)){
+            return null;
+        }
+        const info = fs.readFileSync(this.dbPath,{encoding: 'utf-8'})
+        const data = JSON.parse(info)
+        this.historial =  data.historial
+    }
 }
 
 module.exports = {
